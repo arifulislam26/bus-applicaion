018f629447a4eaab104111023629d9dbb588e44a
Ariful Islam Akash <65933906+arifulislam26@users.noreply.github.com>
2021-06-06 19:31:03 +0600

    Add files via upload

---
+++ final/Transport/src/transport/historydata.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transport;
+
+/**
+ *
+ * @author ARIFUL
+ */
+public class historydata {
+    String name,sit,jdate,jtime,dis,mail,phone,idate;
+
+
+
+    public historydata(String name, String sit, String jdate, String jtime, String dis, String mail, String phone, String idate) {
+        this.name = name;
+        this.sit = sit;
+        this.jdate = jdate;
+        this.jtime = jtime;
+        this.dis = dis;
+        this.mail = mail;
+        this.phone = phone;
+        this.idate = idate;
+    }
+
+       public String getJdate() {
+        return jdate;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getSit() {
+        return sit;
+    }
+
+ 
+
+    public String getJtime() {
+        return jtime;
+    }
+
+    public String getDis() {
+        return dis;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getIdate() {
+        return idate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSit(String sit) {
+        this.sit = sit;
+    }
+
+    public void setJdate(String jdate) {
+        this.jdate = jdate;
+    }
+
+    public void setJtime(String jtime) {
+        this.jtime = jtime;
+    }
+
+    public void setDis(String dis) {
+        this.dis = dis;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setIdate(String idate) {
+        this.idate = idate;
+    }
+
+
+    
+    
+}
